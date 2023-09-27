8c622a89740ff22356e02a2832239d3d14bc1a88
vishnu <shuklavishnu063@gmail.com>
2023-09-27 19:39:34 +0530

    pushing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizAppParentComponent } from './components/quiz-app-parent/quiz-app-parent.component';
+import { QuizAppChildComponent } from './components/quiz-app-child/quiz-app-child.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule }  from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        QuizAppParentComponent,
+        QuizAppChildComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatCardModule,
+        NgSelectModule,
+        FormsModule,
+        MatSlideToggleModule,
+        MatInputModule,
+        MatFormFieldModule
+    ]
 })
 export class AppModule { }
